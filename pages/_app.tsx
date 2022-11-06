d1c6f48b9407d6470d0010c4153506adbafe34a3
Fiils <ioanalexandruipatov12@gmail.com>
2022-11-06 22:36:51 +0200

    modified title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
   <Head>
-    <title>Ipatov Ioan Alexandru - Web Application Engineering</title>
+    <title>Ipatov Ioan Alexandru - Web Application Engineer</title>
     <meta name="description" content="Web Portfolio for Ipatov Ioan Alexandru." />
     <meta name='robots' content='index, nofollow' />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
